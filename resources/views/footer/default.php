b09a9f1225e09145f7810b7b1b7c6b3ffe9d10f6
m-e-h <info@martyhelmick.com>
2018-09-19 15:25:31 -0400

    Borrow some base styles for editing

--- resources/views/footer/default.php
+++ resources/views/footer/default.php
@@ -1,7 +1,7 @@
 <footer class="app-footer">
 
 		<p class="app-footer__credit">
-			<?php esc_html_e( 'Powered by crazy ideas and passion.', 'forsite' ) ?>
+			<?php printf( '<a class="site-link" href="%s" rel="home">%s</a>', esc_url( home_url() ), get_bloginfo( 'name' ) );	?>
 		</p>
 
 	</footer>
