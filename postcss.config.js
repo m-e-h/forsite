71b42af5ee1b27056ef760d77eec6ad9a0e32698
m-e-h <info@martyhelmick.com>
2018-09-25 17:32:13 -0400

    styles and structure

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 module.exports = ctx => ({
-	map: ctx.env === "development" ? true : false,
+	map: ctx.env === "development" ? {inline: false} : false,
 	plugins: {
 		"postcss-import": {},
 		"postcss-simple-vars": {},
 		"postcss-color-function": {},
 		"postcss-preset-env": { stage: 0 },
+		"postcss-extend-rule": {},
 		cssnano: ctx.env === "production" ? { preset: "default" } : false
 	}
 });
