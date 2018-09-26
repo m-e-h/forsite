4740643310f2ce93c71dde1475c2c9b70bc08bbb
m-e-h <info@martyhelmick.com>
2018-09-26 18:20:12 -0400

    moving to vowelcss for styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,15 @@
 module.exports = ctx => ({
-	map: ctx.env === "development" ? {inline: false} : false,
+	map: ctx.env === "development" ? { inline: false } : false,
 	plugins: {
 		"postcss-import": {},
 		"postcss-simple-vars": {},
-		"postcss-color-function": {},
-		"postcss-preset-env": { stage: 0 },
+		"postcss-preset-env": {
+			stage: 0,
+			features: { "color-mod-function": true }
+		},
 		"postcss-extend-rule": {},
+		"postcss-discard-comments": {},
+		"postcss-discard-empty": {},
 		cssnano: ctx.env === "production" ? { preset: "default" } : false
 	}
 });
