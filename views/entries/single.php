ffd30e7a4154dc1f2afd9adb8ed0e7dd80421eaf
m-e-h <info@martyhelmick.com>
2018-10-24 15:54:28 -0400

    move most blocks to general objects

--- views/entries/single.php
+++ views/entries/single.php
@@ -2,7 +2,7 @@
 
 	<?= Hybrid\View\render( 'components', 'post-header' ); ?>
 
-	<div class="entry__content u-content-wrap">
+	<div class="entry__content">
 		<?php the_content() ?>
 		<?= Hybrid\View\render( 'components', 'post-pagination' ); ?>
 	</div>
