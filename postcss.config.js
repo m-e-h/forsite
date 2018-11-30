6d590c672356c9d93d4befa70f81ed0bb0e57cf0
m-e-h <info@martyhelmick.com>
2018-11-30 11:15:50 -0500

    deps update

--- postcss.config.js
+++ postcss.config.js
@@ -1,32 +1,29 @@
 module.exports = ctx => ({
-  map: ctx.env === 'development' ? { inline: false } : false,
-  plugins: {
-    'postcss-import': {},
-    'postcss-simple-vars': {},
-    'postcss-preset-env': {
-      stage: 0,
-      features: {
-        'color-mod-function': true,
-        'all-property': false
-      }
-    },
-    'postcss-extend-rule': {},
-    'postcss-discard-comments': {},
-    'postcss-discard-empty': {},
-    'postcss-scope-to': ctx.env === 'editor'
-            ? {
-			  scopeTo: '.editor-styles-wrapper',
-
-			  suffix: ':not([class^="components-"]):not([class^="editor-"]):not([class^="block-"]):not([aria-owns])',
-
-              remove: [
-                'html',
-                ':disabled',
-                '[readonly]',
-                '[disabled]'
-			  ],
-            }
-            : false,
-    cssnano: ctx.env === 'production' ? { preset: 'default' } : false
-  }
-})
+	map: 'development' === ctx.env ? { inline: false } : false,
+	plugins: {
+		'postcss-import': {},
+		'postcss-simple-vars': {},
+		'postcss-preset-env': {
+			stage: 0,
+			features: {
+				'color-mod-function': true,
+				'all-property': false
+			}
+		},
+		'postcss-extend-rule': {},
+		'postcss-discard-comments': {},
+		'postcss-discard-empty': {},
+		'postcss-editor-styles':
+			'editor' === ctx.env ?
+				{
+					remove: [
+						'html',
+						':disabled',
+						'[readonly]',
+						'[disabled]'
+					]
+				} :
+				false,
+		cssnano: 'production' === ctx.env ? { preset: 'default' } : false
+	}
+});
