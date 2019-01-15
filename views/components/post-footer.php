d49bdb9c6ae10861fad66c6d188cf3677452509d
m-e-h <info@martyhelmick.com>
2019-01-15 17:18:42 -0500

    add edit post link and form styles

--- views/components/post-footer.php
+++ views/components/post-footer.php
@@ -3,4 +3,8 @@
 <footer class="entry__footer u-content-width">
 	<?= Hybrid\Post\render_terms( [ 'taxonomy' => 'category' ] ) ?>
 	<?= Hybrid\Post\render_terms( [ 'taxonomy' => 'post_tag' ] ) ?>
+
+	<?php edit_post_link( __( 'Edit', 'textdomain' ), null, null, null, 'u-abs u-right0 post-edit-link' ); ?>
 </footer>
+
+
