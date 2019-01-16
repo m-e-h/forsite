12d7a46248a0ee7e2dd95306ddf9730d2d208883
m-e-h <info@martyhelmick.com>
2019-01-16 16:50:42 -0500

    block styles

--- views/components/post-info.php
+++ views/components/post-info.php
@@ -1,5 +1,5 @@
 <div class="entry__info">
-	<?= Hybrid\Post\render_author() ?>
+	<?= Hybrid\Post\render_author( ['before' => '<span class="byline-label">By </span>'] ) ?>
 	<?= Hybrid\Post\render_date() ?>
-	<?= Hybrid\Post\render_comments_link( ['after' => '&#x1F4AC;'] ); ?>
+	<?= Hybrid\Post\render_comments_link( ['before' => '&#x1F4AC;'] ); ?>
 </div>
