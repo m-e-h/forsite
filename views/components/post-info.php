f4d99dcec67b723aaaa8f1cf0dbb44e2bf81b348
m-e-h <info@martyhelmick.com>
2019-01-16 11:39:07 -0500

    add some emoji and update edit post link location

--- views/components/post-info.php
+++ views/components/post-info.php
@@ -1,5 +1,5 @@
 <div class="entry__info">
-	<?php Hybrid\Post\display_author() ?>
-	<?php Hybrid\Post\display_date() ?>
-	<?php Hybrid\Post\display_comments_link() ?>
+	<?= Hybrid\Post\render_author() ?>
+	<?= Hybrid\Post\render_date() ?>
+	<?= Hybrid\Post\render_comments_link( ['after' => '&#x1F4AC;'] ); ?>
 </div>
--- views/entries/single.php
+++ views/entries/single.php
@@ -3,9 +3,14 @@
 	<?= Hybrid\View\render( 'components', 'post-header' ); ?>
 
 	<div class="entry__content">
+
 		<?php the_content() ?>
+
 		<?= Hybrid\View\render( 'components', 'post-pagination' ); ?>
+
 	</div>
+
+	<?php edit_post_link( __( 'Edit &#x270F;', 'textdomain' ), '<div class="edit-link-container u-bold u-opacity u-content-width u-px1 u-text-right">', '</div>' ); ?>
 
 	<?= Hybrid\View\render( 'components', 'post-footer' ); ?>
 
