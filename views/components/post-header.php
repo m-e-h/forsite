02a8b5159cbc43a6be44e9a9264b2d5ae59edcc8
m-e-h <info@martyhelmick.com>
2019-04-14 17:20:02 -0400

    vertical rhythm

--- views/components/post-header.php
+++ views/components/post-header.php
@@ -1,6 +1,6 @@
 <?php if ( is_home() || is_front_page() ) {	return; } ?>
 
-<header class="entry__header u-mb1">
+<header class="entry__header">
 
 	<?= Hybrid\Post\render_terms(
 		[
@@ -10,7 +10,7 @@
 		]
 	) ?>
 
-	<?= Hybrid\Post\render_title( [ 'class' => 'entry__title u-h1 u-m0' ] ); ?>
+	<?= Hybrid\Post\render_title( [ 'class' => 'entry__title u-h1' ] ); ?>
 
 	<?php if ( is_singular( 'post' ) ) : ?>
 
--- views/layouts/index.php
+++ views/layouts/index.php
@@ -3,7 +3,7 @@
 	<?= Hybrid\View\render( 'components', 'featured-image' ); ?>
 
 	<header class="archive-entry__header u-m05">
-		<?= Hybrid\Post\render_title( [ 'class' => 'entry__title u-h3 u-m0' ] ); ?>
+		<?= Hybrid\Post\render_title( [ 'class' => 'entry__title u-h3' ] ); ?>
 	</header>
 
 	<?php the_excerpt() ?>
