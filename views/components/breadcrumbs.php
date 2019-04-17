933f72b6eaf1e706fb8d8b629444646f26fa4969
m-e-h <info@martyhelmick.com>
2019-04-17 18:07:56 -0400

    show breadcrumbs by default

--- views/components/breadcrumbs.php
+++ views/components/breadcrumbs.php
@@ -2,7 +2,7 @@
 <?= Hybrid\Breadcrumbs\Trail::render( $args = [
 	'list_tag'      => 'ol',
 	'show_trail_end' => true,
-	'container_class' => get_theme_mod( 'forsite_breadcrumbs' ) ? 'breadcrumbs' : 'breadcrumbs screen-reader-text',
+	'container_class' => get_theme_mod( 'forsite_breadcrumbs', true ) ? 'breadcrumbs' : 'breadcrumbs screen-reader-text',
 	'labels' => [
         'title' => ''
     ]
