e139d3deaf8e42a3c60eb376bcacbaca4daf9849
m-e-h <info@martyhelmick.com>
2019-04-18 17:10:05 -0400

    print styles

--- views/layouts/single.php
+++ views/layouts/single.php
@@ -8,7 +8,7 @@
 
 		<?= Hybrid\View\render( 'components', 'post-pagination' ); ?>
 
-	<?php edit_post_link( __( 'Edit &#x270F;', 'textdomain' ), '<div class="edit-link-container u-bold u-opacity u-content-width u-px1 u-text-right">', '</div>' ); ?>
+	<?php edit_post_link( __( 'Edit &#x270F;', 'textdomain' ), '<div class="edit-link-container u-print-none u-bold u-opacity u-content-width u-px1 u-text-right">', '</div>' ); ?>
 
 	<?= Hybrid\View\render( 'components', 'post-footer' ); ?>
 
