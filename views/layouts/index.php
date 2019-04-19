d3d12b4e2a2de89a827a75a202644fb38f04bb79
m-e-h <info@martyhelmick.com>
2019-04-19 17:53:42 -0400

    archive layout links

--- views/layouts/index.php
+++ views/layouts/index.php
@@ -1,11 +1,29 @@
-<article <?= Hybrid\Attr\render( 'archive-entry' ) ?>>
+<article <?= Hybrid\Attr\render( 'archive-entry' ); ?>>
 
 	<?= Hybrid\View\render( 'components', 'featured-image' ); ?>
 
 	<header class="archive-entry__header">
-		<?= Hybrid\Post\render_title( [ 'class' => 'entry__title u-h3' ] ); ?>
+		<?= Hybrid\Post\render_title(
+			[
+				'class' => 'entry__title u-h3',
+				'link'  => false,
+			]
+		); ?>
 	</header>
 
-	<?php the_excerpt() ?>
-
+	<?php
+	the_content(
+		sprintf(
+			wp_kses(
+				__( '<span class="ellipsis-more">&#8230;</span><span class="screen-reader-text"> "%s"</span>', '_s' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		)
+	);
+	?>
 </article>
